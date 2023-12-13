fa921d3018a0c2a449bc7cf06f3116a6a8dbf7e8
Geace <kanmorning@gmail.com>
2023-12-14 02:30:38 +0800

    first commit

---
+++ app/_components/DemoSlider.js
@@ -0,0 +1,73 @@
+"use client"; // <===== REQUIRED
+
+import React from "react";
+import Image from "next/image";
+
+// Swiper components, modules and styles
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const DemoSlider = ({ data }) => {
+  console.log(data);
+  return (
+    <section className="w-full">
+      <div className=" h-screen">
+        <ul className="h-full w-full">
+          <Swiper
+            navigation
+            pagination={{ type: "bullets", clickable: true }}
+            // autoplay={true}
+            loop={true}
+            modules={[Autoplay, Navigation, Pagination]}
+          >
+            {data.map(
+              ({ id, title, image, time, link, distance, pro, con }) => (
+                <SwiperSlide key={id}>
+                  <div className="flex justify-between items-center text-white bg-orange-600 text-lg px-8 py-4">
+                    <p>{title}</p>
+                    <p className="cursor-pointer">
+                      <a href={link} target="_blank">
+                        <Image
+                          src="/search-icon.png"
+                          width={20}
+                          height={20}
+                          alt=""
+                        />
+                      </a>
+                    </p>
+                  </div>
+                  <div className="px-8 py-2">
+                    <div>
+                      <span>營業時間</span>
+                      {`：${time}`}
+                    </div>
+                    <div>
+                      <span>距離</span>
+                      {`：${distance} 分`}
+                    </div>
+                    <div>
+                      <span>優點</span>
+                      {`：${pro}`}
+                    </div>
+                    <div className="pb-4">
+                      <span>缺點</span>
+                      {`：${con}`}
+                    </div>
+                    <div className="w-4/5">
+                      <Image src={image} width={300} height={300} alt="" />
+                    </div>
+                  </div>
+                </SwiperSlide>
+              )
+            )}
+          </Swiper>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default DemoSlider;
---
+++ app/_data/slider-data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    id: 1,
+    title: "A.雞肉先生",
+    image: "/menu-a.jpeg",
+    time: "11:30-20:30",
+    link: "https://maps.app.goo.gl/CdtsBFMTW25dGmf47",
+    distance: "4",
+    pro: "運動後補充蛋白質",
+    con: "只開到八點半，時間有點緊迫",
+  },
+  {
+    id: 2,
+    title: "B.阿泉小吃什錦麵鵝肉熱炒",
+    image: "/menu-b.jpeg",
+    time: "16:00-00:00",
+    link: "https://maps.app.goo.gl/dPDUM9ZAveaU8j4o7",
+    distance: "5",
+    pro: "開到晚上 12 點可以慢慢吃",
+    con: "",
+  },
+];
+
+export default data;
