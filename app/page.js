1d0a54786d7cd2eecc9b8dd88d82a77d73758d03
Geace <kanmorning@gmail.com>
2023-12-21 01:28:15 +0800

    update text

--- app/page.js
+++ app/page.js
@@ -4,7 +4,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex mt-4 min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div className="mb-2 w-full h-6 bg-gray-300 text-center">雙打</div>
       <div>-&nbsp;發球&nbsp;-</div>
       <Image src="/doubles-serve.jpg" width={300} height={30} alt="" />
       <div>-&nbsp;發球後&nbsp;-</div>
