1d3288bf1f579914ebf4024e60354b9731bf9b3b
Geace <kanmorning@gmail.com>
2023-12-21 01:15:48 +0800

    add double rule

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
 import DemoSlider from "./_components/DemoSlider";
 import dataSlider from "./_data/slider-data";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex mt-4 min-h-screen flex-col items-center justify-between">
+      <div>-&nbsp;發球&nbsp;-</div>
+      <Image src="/doubles-serve.jpg" width={300} height={30} alt="" />
+      <div>-&nbsp;發球後&nbsp;-</div>
+      <Image src="/doubles-after-serve.jpg" width={300} height={30} alt="" />
       <DemoSlider data={dataSlider} />
     </main>
   );
