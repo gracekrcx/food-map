e850a1895a01be21cdf1fd02bc5b26a99a3195fd
Lisa <kanmorning@gmail.com>
2023-12-21 16:31:01 +0800

    test update

--- app/page.js
+++ app/page.js
@@ -1,15 +1,15 @@
-import DemoSlider from "./_components/DemoSlider";
-import dataSlider from "./_data/slider-data";
-import Image from "next/image";
+import DemoSlider from './_components/DemoSlider';
+import dataSlider from './_data/slider-data';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className="mb-2 w-full h-6 bg-gray-300 text-center">雙打</div>
+    <main className='flex min-h-screen flex-col items-center justify-between'>
+      <div className='mb-2 w-full h-6 bg-gray-300 text-center'>雙打</div>
       <div>-&nbsp;發球&nbsp;-</div>
-      <Image src="/doubles-serve.jpg" width={300} height={30} alt="" />
-      <div>-&nbsp;發球後&nbsp;-</div>
-      <Image src="/doubles-after-serve.jpg" width={300} height={30} alt="" />
+      <Image src='/doubles-serve.jpg' width={300} height={30} alt='' />
+      <div>-&nbsp;發球後&nbsp;--</div>
+      <Image src='/doubles-after-serve.jpg' width={300} height={30} alt='' />
       <DemoSlider data={dataSlider} />
     </main>
   );
